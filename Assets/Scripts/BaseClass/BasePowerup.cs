aefe9d28f88c99d850bdbfa342bba145f442f28d
miran2511 <kimmiran2511@gmail.com>
2023-10-20 04:51:10 +0800

    add scripts

---
+++ Assets/Scripts/BaseClass/BasePowerup.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public abstract class BasePowerup : MonoBehaviour
+{
+    // public PowerupType type;
+    public bool spawned = false;
+    protected bool consumed = false;
+    protected bool goRight = true;
+    protected Rigidbody2D rigidBody;
+    protected Vector3 powerupPos;
+
+    // base methods
+    protected virtual void Start()
+    {
+        powerupPos = GetComponent<Transform>().position;
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // interface methods
+    // 1. concrete methods
+    // public PowerupType powerupType
+    // {
+    //     get // getter
+    //     {
+    //         return type;
+    //     }
+    // }
+
+    public bool hasSpawned
+    {
+        get // getter
+        {
+            return spawned;
+        }
+    }
+
+    public void DestroyPowerup()
+    {
+        this.gameObject.transform.position = powerupPos;
+        GetComponentInChildren<SpriteRenderer>().enabled = false;
+        rigidBody.velocity = Vector2.zero;
+    }
+
+    // 2. abstract methods, must be implemented by derived classes
+    public abstract void SpawnPowerup();
+    public abstract void ApplyPowerup(MonoBehaviour i);
+}
